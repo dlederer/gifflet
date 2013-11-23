fa876be85659e038c8196702c72fe3b484b88190
David Lederer <Dlederer@david-lederers-macbook-pro-3.local>
2013-11-22 22:50:40 -0500

    First Commit.

---
+++ app/controllers/gifflet_homepage_controller.rb
@@ -0,0 +1,7 @@
+class GiffletHomepageController < ApplicationController
+
+	def index
+
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gifflet::Application.config.secret_token = 'c7cc60c3159ac57cf94e635d2cf7566f47cfe328f29c5f2593cc8b5f3cea5b91da8bb978f8d54a5f0d3adf25b91dd83004f92f3fa68577dfa31e40c670bdb09a'
